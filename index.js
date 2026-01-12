6a2a6fce3943c56e5f04531d31b0d6fcbcc51667
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-12 11:57:21 +0100

    posts

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
   .then((data) => {
-    const limitedData = data.slice(0, 5);
-    console.log(limitedData);
+    const postsArray = data.slice(0, 5);
+
+    const html = postsArray
+      .map((post) => {
+        return `<div class="post">
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                    <hr />
+                </div>`;
+      })
+      .join("");
+
+    document.getElementById("post-container").innerHTML = html;
   });
